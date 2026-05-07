ac17f13c4d2f3047ba71271ce195702881eb8c6a
sofiameridmon <sofmermon@alum.us.es>
2026-05-07 13:51:21 +0200

    springboot

---
+++ src/main/java/aiss/twitchMiner/TwitchMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.twitchMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchMinerApplication.class, args);
+	}
+
+}
